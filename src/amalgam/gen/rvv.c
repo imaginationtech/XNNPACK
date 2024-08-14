957e51285e64782d98935b7121e77d55e6c2272b
Mihai.Olinovici <mihai.olinovici@imgtec.com>
2024-08-14 09:02:23 +0000

    Add RVV qs8/qu8-f32-vcvt config.

--- src/amalgam/gen/rvv.c
+++ src/amalgam/gen/rvv.c
@@ -21,6 +21,7 @@
 #include "xnnpack/raddstoreexpminusmax.h"
 #include "xnnpack/transpose.h"
 #include "xnnpack/vbinary.h"
+#include "xnnpack/vcvt.h"
 #include "xnnpack/vunary.h"
 
 
@@ -1597,6 +1598,37 @@ void xnn_f32_vrsqrt_ukernel__rvv_rsqrt_u4v(
   }
 }
 
+void xnn_qs8_f32_vcvt_ukernel__rvv_u2v(
+    size_t batch,
+    const int8_t* input,
+    float* output,
+    const union xnn_qs8_f32_cvt_params params[restrict XNN_MIN_ELEMENTS(1)]) XNN_OOB_READS
+{
+  assert(batch != 0);
+  assert(batch % sizeof(int8_t) == 0);
+  assert(input != NULL);
+  assert(output != NULL);
+
+  batch >>= XNN_LOG2_SIZEOF_INT8_T;
+
+  const float vscale = params->rvv.scale;
+
+  const int32_t minus_zero_point = params->rvv.minus_zero_point;
+  float *o = output;
+  for (; batch > 0; ) {
+    const int32_t n = __riscv_vsetvl_e8m2(batch); batch -= n;
+
+    vint8m2_t x_i8v = __riscv_vle8_v_i8m2(input, n); input += n;
+
+    vint32m8_t wx_i32v = __riscv_vsext_vf4_i32m8(x_i8v, n);
+    wx_i32v = __riscv_vadd_vx_i32m8(wx_i32v, minus_zero_point, n);
+    vfloat32m8_t y_f32v = __riscv_vfcvt_f_x_v_f32m8(wx_i32v, n);
+    y_f32v = __riscv_vfmul_vf_f32m8(y_f32v, vscale, n);
+
+    __riscv_vse32_v_f32m8(o, y_f32v, n); o += n;
+  }
+}
+
 void xnn_qs8_vmul_minmax_fp32_ukernel__rvv_u2v(
     size_t batch,
     const int8_t* input_a,
@@ -1683,6 +1715,37 @@ void xnn_qs8_vmulc_minmax_fp32_ukernel__rvv_u2v(
 
     batch -= n;
   } while (batch != 0);
+}
+
+void xnn_qu8_f32_vcvt_ukernel__rvv_u2v(
+    size_t batch,
+    const uint8_t* input,
+    float* output,
+    const union xnn_qu8_f32_cvt_params params[restrict XNN_MIN_ELEMENTS(1)]) XNN_OOB_READS
+{
+  assert(batch != 0);
+  assert(batch % sizeof(int8_t) == 0);
+  assert(input != NULL);
+  assert(output != NULL);
+
+  batch >>= XNN_LOG2_SIZEOF_INT8_T;
+
+  const float vscale = params->rvv.scale;
+
+  const int32_t minus_zero_point = params->rvv.minus_zero_point;
+  float *o = output;
+  for (; batch > 0; ) {
+    const int32_t n = __riscv_vsetvl_e8m2(batch); batch -= n;
+
+    vuint8m2_t x_u8v = __riscv_vle8_v_u8m2(input, n); input += n;
+
+    vint32m8_t wx_i32v = __riscv_vreinterpret_v_u32m8_i32m8(__riscv_vzext_vf4_u32m8(x_u8v, n));
+    wx_i32v = __riscv_vadd_vx_i32m8(wx_i32v, minus_zero_point, n);
+    vfloat32m8_t y_f32v = __riscv_vfcvt_f_x_v_f32m8(wx_i32v, n);
+    y_f32v = __riscv_vfmul_vf_f32m8(y_f32v, vscale, n);
+
+    __riscv_vse32_v_f32m8(o, y_f32v, n); o += n;
+  }
 }
 
 void xnn_qu8_vmul_minmax_fp32_ukernel__rvv_u2v(
